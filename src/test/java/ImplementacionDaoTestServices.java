b47c8a5f701488af5598aefd04f00f5f44f1a211
luiscarlos27 <c32veg27@gmail.com>
2016-03-23 10:33:04 -0600

    creacion de dao test de implementado

--- src/test/java/ImplementacionDaoTestServices.java
+++ src/test/java/ImplementacionDaoTestServices.java
@@ -4,20 +4,38 @@
  * and open the template in the editor.
  */
 
+import com.soft.primerproyecto.Dao.SucursalDao;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
 
 /**
  *
  * @author Usuario
  */
-public class ImplementacionDaoTestServices {
+public class ImplementacionDaoTestServices extends AbstrantTransactionalTestNGSpringContextTests{
+    
+    private static Logger log = Logger.getLogger(SucursalDao.class);
+    @Autowired
+    private SucursalDao sucursal;
     
     public ImplementacionDaoTestServices() {
+    }
+    
+    @BeforeClass
+    public void initDataBase() {
+        String fileName = "sql/script.sql";
+        Resource resource = applicationContext.getResource(fileName);
+        if (resource.exists()) {
+            executeSqlScript(fileName, false);
+        } else {
+            log.debug("No existe el archivo: {}" + resource);
+        }
     }
     
     @BeforeClass
